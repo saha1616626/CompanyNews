366a5f8993d6e5738d5715a275775c8f7fbb3f46
Березкин Александр <3.commm@gmail.com>
2024-12-10 11:19:13 +0300

    Добавили класс определяющий состояние учетной записи в текущей сессии

---
+++ CompanyNews/Models/Authorization/UserLoginStatus.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyNews.Models.Authorization
+{
+	/// <summary>
+	/// Состояние входа пользователя
+	/// </summary>
+	public class UserLoginStatus
+	{
+		/// <summary>
+		/// Вошел ли пользователь в систему?
+		/// </summary>
+		public bool isUserLoggedIn {  get; set; }
+
+		/// <summary>
+		/// Учетная запись пользователя
+		/// </summary>
+		public int? accountId { get; set; }
+
+		/// <summary>
+		/// Роль пользователя в системе
+		/// </summary>
+		public string? accountRole { get; set; }
+
+
+	}
+}
