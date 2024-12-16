7f7b755ef15c85b14682a124f250e5ffc18fb3fe
Березкин Александр <3.commm@gmail.com>
2024-12-16 14:47:51 +0300

    Создание страницы WorkDepartmentWorkingPage и настройка связи с ViewModel

---
+++ CompanyNews/ViewModels/AdminApp/WorkingWithData/WorkDepartmentWorkingViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CompanyNews.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyNews.ViewModels.AdminApp.WorkingWithData
+{
+	public class WorkDepartmentWorkingViewModel : INotifyPropertyChanged
+	{
+		/// <summary>
+		/// Сервисы для взаиодействия с бизнес-логикой
+		/// </summary>
+		private readonly NewsCategoryService _newsCategoryService; // Категория
+		private readonly WorkDepartmentService _workDepartmentService; // Рабочий отдел
+
+		public WorkDepartmentWorkingViewModel()
+		{
+			_newsCategoryService = ServiceLocator.GetService<NewsCategoryService>();
+			_workDepartmentService = ServiceLocator.GetService<WorkDepartmentService>();
+
+
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
---
+++ CompanyNews/Views/AdminApp/WorkingWithData/WorkDepartmentWorkingPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using CompanyNews.Helpers.Event;
+using CompanyNews.ViewModels.AdminApp.WorkingWithData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CompanyNews.Views.AdminApp.WorkingWithData
+{
+	/// <summary>
+	/// Interaction logic for WorkDepartmentWorkingPage.xaml
+	/// </summary>
+	public partial class WorkDepartmentWorkingPage : Page
+	{
+		/// <summary>
+		/// Объект класса
+		/// </summary>
+		private readonly WorkDepartmentWorkingViewModel _workDepartmentWorkingViewModel;
+
+		public WorkDepartmentWorkingPage()
+		{
+			InitializeComponent();
+
+			_workDepartmentWorkingViewModel = (WorkDepartmentWorkingViewModel)this.Resources["WorkDepartmentWorkingViewModel"];
+		}
+
+		/// <summary>
+		/// Закрываем "гамбургер" меню, если открыто, при нажатии на окно, но не на меню
+		/// </summary>
+		private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			HamburgerMenuEvent.CloseHamburgerMenu();
+		}
+	}
+}
